312af9de538ba3d85fb14303ad89d8ff4e4428b6
Denjoel <joeldennis66@gmail.com>
2023-09-08 23:59:11 +0300

    3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - This is a function
+ * Return: Always 0
+ */
+int main(void)
+
+{
+	char j;
+
+	for (j = 'a'; j <= 'z'; j++)
+	putchar(j);
+
+	for (j = 'A'; j <= 'Z'; j++)
+	putchar(j);
+	putchar('\n');
+
+	return (0);
+}
