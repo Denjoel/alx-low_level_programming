3651510659c91f2cbe0dcf433c250bb7cc2f462e
Denjoel <joeldennis66@gmail.com>
2023-09-08 23:42:48 +0300

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - a function
+ * Return: 0
+ */
+
+int main(void)
+
+{
+	char j;
+
+	for (j = 'a'; j <= 'z' ; j++)
+	putchar(j);
+	putchar('\n');
+
+	return (0);
+}
+
