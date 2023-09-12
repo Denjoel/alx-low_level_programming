094014860cf3211ce28945fcdd3edc669cdef35c
Denjoel <joeldennis66@gmail.com>
2023-09-12 19:04:21 +0300

    alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_alphabet - this is the function
+ * Return: Always 0 Sucess
+ */
+
+void print_alphabet(void)
+
+{
+	char d;
+
+	for (d = 'a'; d <= 'z'; d++)
+	{
+		_putchar(d);
+	}
+	_putchar('\n');
+}
